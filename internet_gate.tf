faf9f06f859ecf2243fb794f3a2c3ebb992bc2fc
SeredaYehor <sirjeleshka@gmail.com>
2022-04-29 00:51:13 +0300

    Removed old tf files

--- internet_gate.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_internet_gateway" "internet_gw" {
-  vpc_id = aws_vpc.main.id
-
-  tags = {
-    Name = "internet_gw"
-  }
-}
-
--- vpc.tf
+++
@@ -1,5 +0,0 @@
-resource "aws_vpc" "main" {
-   cidr_block = "172.64.0.0/23"
-}
-
-
