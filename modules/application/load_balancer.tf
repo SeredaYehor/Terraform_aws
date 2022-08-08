b4fbe48c164450a1dc44dc0c9bd9dd8fd77c98f4
SeredaYehor <sirjeleshka@gmail.com>
2022-08-08 11:54:07 +0300

    Fixed forwarding and added redirect to https

--- modules/application/load_balancer.tf
+++ modules/application/load_balancer.tf
@@ -1,14 +1,16 @@
 resource "aws_lb" "load-balancer" {
   name 			= "public-load-balancer"
   load_balancer_type	= "application"
-  security_groups    	= [aws_security_group.nextcloud_sg.id]
+  security_groups    	= [ aws_security_group.lb-sg.id, aws_security_group.nextcloud_sg.id ]
   subnets		= [ var.public_sub_id, var.private_sub_id ] 
 }
 
 resource "aws_lb_listener" "this" {
   load_balancer_arn 	= aws_lb.load-balancer.arn
-  protocol		= "HTTP"
-  port			= "80"
+  protocol		= "HTTPS"
+  port			= "443"
+  certificate_arn       = var.certificate
+
   default_action {
     type		= "forward"
     target_group_arn	= aws_lb_target_group.nextcloud-target.arn
@@ -20,6 +22,10 @@ resource "aws_lb_target_group" "nextcloud-target" {
   protocol 	= "HTTP"
   vpc_id	= var.vpc_id
   depends_on = [ aws_lb.load-balancer ]
+
+  health_check {
+    matcher  = "200,400,302"
+  }
 }
 
 resource "aws_lb_target_group_attachment" "nextcloud_attach" {
@@ -28,3 +34,18 @@ resource "aws_lb_target_group_attachment" "nextcloud_attach" {
   port             = 80
 }
 
+resource "aws_lb_listener" "redirect" {
+  load_balancer_arn = aws_lb.load-balancer.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type = "redirect"
+
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+    }
+  }
+}
