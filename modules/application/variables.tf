f60e26288dde3cb7481321270198b97bdc5d4e77
SeredaYehor <sirjeleshka@gmail.com>
2022-08-08 11:56:12 +0300

    Added vars for dns and https

--- modules/application/variables.tf
+++ modules/application/variables.tf
@@ -1,8 +1,20 @@
 variable "key_name" {
-  default = "your_ssh_access_key_name"
+  default = "ec2_ssh_key_name"
+}
+
+variable "certificate" {
+  default = "aws_cert_arn"
+}
+
+variable "record_name" {
+  default = "your_domain"
 }
 
 variable "vpc_id" {
+}
+
+variable "hosted_zone_id" {
+  default = "zone_id"
 }
 
 variable "private_sub_id" {
