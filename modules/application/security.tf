abf7421b01e16e342fb1479b46398593e9e85938
SeredaYehor <sirjeleshka@gmail.com>
2022-08-08 11:54:44 +0300

    Added sg for https traffic

--- modules/application/security.tf
+++ modules/application/security.tf
@@ -14,15 +14,35 @@ resource "aws_security_group" "nextcloud_sg" {
     from_port           = 80
     to_port             = 80
     protocol            = "tcp"
-    cidr_blocks         = ["0.0.0.0/0"]
-    ipv6_cidr_blocks    = ["::/0"]
+    security_groups     = [ aws_security_group.lb-sg.id ]
   }
 
-  
   egress {
     from_port		= 0
     to_port		= 0
     protocol		= "-1"
+    cidr_blocks         = ["0.0.0.0/0"]
+    ipv6_cidr_blocks    = ["::/0"]
+  }
+}
+
+resource "aws_security_group" "lb-sg" {
+  name        = "load-balancer-sg"
+  description = "SG for load balancer https target group"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    from_port          = 443
+    to_port            = 443
+    protocol           = "tcp"
+    cidr_blocks        = ["0.0.0.0/0"]
+    ipv6_cidr_blocks   = ["::/0"]
+  }
+
+  egress {
+    from_port           = 0
+    to_port             = 0
+    protocol            = "-1"
     cidr_blocks         = ["0.0.0.0/0"]
     ipv6_cidr_blocks    = ["::/0"]
   }
