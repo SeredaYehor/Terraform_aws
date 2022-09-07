96789f651a4ceb7fc82f81c77a13a094092b4059
SeredaYehor <sirjeleshka@gmail.com>
2022-09-08 00:41:01 +0300

    Added counter as if statement for enabling vpn mode

--- modules/application/dns_record.tf
+++ modules/application/dns_record.tf
@@ -1,7 +1,8 @@
 resource "aws_route53_record" "nextcloud_record" {
+  count           = var.vpn_mode == "false" ? 1 : 0
   allow_overwrite = true
   name            = var.record_name
-  records         = [ aws_lb.load-balancer.dns_name ]
+  records         = [ aws_lb.load-balancer[count.index].dns_name ]
   type            = "CNAME"
   zone_id         = var.hosted_zone_id
   ttl             = 9600
--- modules/application/load_balancer.tf
+++ modules/application/load_balancer.tf
@@ -1,4 +1,5 @@
 resource "aws_lb" "load-balancer" {
+  count                 = var.vpn_mode == "false" ? 1 : 0
   name 			= "public-load-balancer"
   load_balancer_type	= "application"
   security_groups    	= [ aws_security_group.lb-sg.id, aws_security_group.nextcloud_sg.id ]
@@ -6,18 +7,20 @@ resource "aws_lb" "load-balancer" {
 }
 
 resource "aws_lb_listener" "this" {
-  load_balancer_arn 	= aws_lb.load-balancer.arn
+  count                 = var.vpn_mode == "false" ? 1 : 0
+  load_balancer_arn 	= aws_lb.load-balancer[count.index].arn
   protocol		= "HTTPS"
   port			= "443"
   certificate_arn       = var.certificate
 
   default_action {
     type		= "forward"
-    target_group_arn	= aws_lb_target_group.nextcloud-target.arn
+    target_group_arn	= aws_lb_target_group.nextcloud-target[count.index].arn
   }
 }
 
 resource "aws_lb_target_group" "nextcloud-target" {
+  count         = var.vpn_mode == "false" ? 1 : 0
   port		= "80"
   protocol 	= "HTTP"
   vpc_id	= var.vpc_id
@@ -29,13 +32,15 @@ resource "aws_lb_target_group" "nextcloud-target" {
 }
 
 resource "aws_lb_target_group_attachment" "nextcloud_attach" {
-  target_group_arn = aws_lb_target_group.nextcloud-target.arn
-  target_id        = aws_instance.nextcloud.id
+  count                 = var.vpn_mode == "false" ? 1 : 0
+  target_group_arn = aws_lb_target_group.nextcloud-target[count.index].arn
+  target_id        = var.nextcloud_id
   port             = 80
 }
 
 resource "aws_lb_listener" "redirect" {
-  load_balancer_arn = aws_lb.load-balancer.arn
+  count                 = var.vpn_mode == "false" ? 1 : 0
+  load_balancer_arn = aws_lb.load-balancer[count.index].arn
   port              = "80"
   protocol          = "HTTP"
 
