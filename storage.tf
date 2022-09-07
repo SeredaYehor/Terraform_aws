847ebe821c5ea3ad616b580af76cfb658300c719
SeredaYehor <sirjeleshka@gmail.com>
2022-09-08 00:36:22 +0300

    Updated variables declaration

--- storage.tf
+++ storage.tf
@@ -4,5 +4,5 @@ module "storage" {
   public_sub_id  = module.virtual_cloud.public-subnet
   private_sub_id = module.virtual_cloud.private-subnet
   vpc_id 	 = module.virtual_cloud.vpc_id
-  nextcloud_sg   = module.application.nextcloud_sg
+  nextcloud_sg   = module.application.nextcloud_sg_id
 }
