9f02bedc9087c569206c3bcf04799f848f17a329
SeredaYehor <sirjeleshka@gmail.com>
2022-09-08 00:41:45 +0300

    Updated outputs for instances module

--- modules/application/outputs.tf
+++ modules/application/outputs.tf
@@ -1,11 +1,15 @@
-output "bastion_ip" {
-  value = aws_instance.bastion.public_ip
-}
-
-output "nextcloud_ip" {
-  value = aws_instance.nextcloud.private_ip
-}
-
-output "nextcloud_sg" {
+output "nextcloud_sg_id" {
   value = aws_security_group.nextcloud_sg.id
+}
+
+output "vpn_sg_id" {
+  value = aws_security_group.vpn_sg[0].id
+}
+
+output "lb_sg_id" {
+  value = aws_security_group.lb-sg.id
+}
+
+output "iam_role" {
+  value = aws_iam_instance_profile.vpn-s3-role.name
 }
